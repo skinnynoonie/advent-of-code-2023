aae3443bea698300ac8fda744c6c684e211d5af3
skinnynoonie <101953182+skinnynoonie@users.noreply.github.com>
2023-12-01 08:51:44 -0500

    Update partOne.py

--- day1/partOne.py
+++ day1/partOne.py
@@ -1,1 +1,14 @@
+input = open("input.txt").readlines()
 
+result = 0
+for line in input:
+    first_found_value = None
+    last_found_value = None
+    for char in line:
+        if char.isdigit():
+            if first_found_value == None:
+                first_found_value = int(char)
+            last_found_value = int(char)
+    result = result + int(str(first_found_value) + str(last_found_value))
+
+print("Result: " + str(result))
